89751372008a961a776a50e220618e8949e9128b
Ravnicore <kevin.m.kelly@live.com>
2018-10-07 17:11:05 -0400

    Updates

---
+++ precache-manifest.501e0330cf948a389434d7b282749725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "909cec594173439f474b",
+    "url": "/Star-Wars-API-Learning/static/js/runtime~main.909cec59.js"
+  },
+  {
+    "revision": "e45fee5bdba4c0992ced",
+    "url": "/Star-Wars-API-Learning/static/js/main.e45fee5b.chunk.js"
+  },
+  {
+    "revision": "60c8f97e10b6adcb7db6",
+    "url": "/Star-Wars-API-Learning/static/js/1.60c8f97e.chunk.js"
+  },
+  {
+    "revision": "e45fee5bdba4c0992ced",
+    "url": "/Star-Wars-API-Learning/static/css/main.b05569f1.chunk.css"
+  },
+  {
+    "revision": "60c8f97e10b6adcb7db6",
+    "url": "/Star-Wars-API-Learning/static/css/1.3dc5b497.chunk.css"
+  },
+  {
+    "revision": "dfe1399eb73b1f2cf2e60bd75b6dff28",
+    "url": "/Star-Wars-API-Learning/index.html"
+  }
+];
